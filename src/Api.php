ac30f344763967196a190f504a4da333cef22bb8
Adam Schubert <adam.schubert@sg1-game.net>
2017-09-07 02:12:59 +0200

    Fix

---
+++ src/Api.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sadam
+ * Date: 7.9.17
+ * Time: 1:51
+ */
+
+namespace Salamek\Zasilkovna;
+
+
+abstract class Api
+{
+    private $jsonEndpoint = 'http://www.zasilkovna.cz/api/v3/%s/branch.json';
+
+    public function __construct($apiPassword, $apiKey)
+    {
+        $this->apiKey = $apiKey;
+
+        $this->jsonEndpoint = sprintf($this->jsonEndpoint, $this->apiKey);
+    }
+
+    public function getBranchList()
+    {
+        $result = file_get_contents($this->jsonEndpoint);
+        if (!$result)
+        {
+            throw new \Exception('Failed to open JSON endpoint');
+        }
+
+        $data = json_decode($result);
+        if (!$data)
+        {
+            throw new \Exception('Failed to decode JSON');
+        }
+
+        return $data;
+    }
+}
