f98f43ad40ee3156bad53fbb9817de0c022c151c
Adam Schubert <adam.schubert@sg1-game.net>
2022-04-19 16:34:01 +0200

    fix(PacketAttributesFault): Drop mixed type from constructor, Fixes #25

--- src/Exception/PacketAttributesFault.php
+++ src/Exception/PacketAttributesFault.php
@@ -10,7 +10,7 @@ final class PacketAttributesFault extends \Exception
     private array $fails = [];
 
 
-    public function __construct(mixed $fails)
+    public function __construct($fails)
     {
         // Normalize schema
         if (isset($fails['name'])) {
